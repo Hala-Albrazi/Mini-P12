052af9c821954c82c67becaddec4e2850d2a226d
Hala Albrazi <hala.albarazii@gmail.com>
2025-11-27 07:38:17 +0200

    Update P12_Python.py

--- P12_Python.py
+++ P12_Python.py
@@ -1,6 +1,7 @@
 print("Welcome to Multiplication table:")
-NU = int(input("Please enter a number \n"))
+NU = int(input("Please enter a number: \n"))
 print(f"Here is the multiplication table for number {NU}")
 for i in range(1, 21):
     result = NU * i
     print(f"{NU} x {i} = {result}")
+
